b74210305617b0734aa09aafa8b9c0f7ba9ab0af
cristian-israel <israel.dev@icloud.com>
2025-03-27 23:44:57 -0300

    adiciona monitoramento de mensagens editadas e excluídas no bot do WhatsApp

--- src/app/handlers.ts
+++ src/app/handlers.ts
@@ -1,24 +1,43 @@
 import { Client, Message } from "whatsapp-web.js";
-import { generateQRCode } from "./messages";
+import { generateQRCode, handleMessage } from "./messages";
 
 export function setupHandlers(client: Client) {
-  // Gera QR Code
   client.on("qr", generateQRCode);
 
-  // Conexão estabelecida
   client.once("ready", () => {
     console.log("✅ Bot conectado ao WhatsApp!");
   });
 
-  // Escuta mensagens recebidas
-  client.on("message", async (message) => {
-    if (message.type !== "chat") return;
+  client.on("message", async (message: Message) => {
+    if (
+      message.type !== "chat" ||
+      message.isEphemeral ||
+      message.isForwarded ||
+      message.isGif ||
+      message.isStarred ||
+      message.isStatus
+    )
+      return;
 
-    console.log(`📩 Mensagem recebida: ${message.body}`);
+    // console.log(`📩 - ${message.from}: ${message.body}`);
     await handleMessage(client, message);
   });
-}
-function handleMessage(client: Client, message: Message) {
-  throw new Error("Function not implemented.");
-}
 
+  // Monitoramento de mensagens editadas
+  client.on("message_edit", async (message: Message, newBody: string, prevBody: string) => {
+    if (message.fromMe || message.type !== "chat") return;
+    console.log(`📝 - ${message.from}: ${prevBody} -> ${newBody}`);
+  });
+
+  // Monitoramento de mensagens excluídas
+  client.on("message_revoke_everyone", async (message: Message, revoked_msg: Message) => {
+    if (message.fromMe || message.type !== "chat") return;
+
+    if (revoked_msg) {
+      console.log(`🗑️ - ${message.from}: ${revoked_msg}`);
+    } else {
+      console.log(`🗑️ - ${message.from}: {A mensagem não pôde ser recuperada.}`);
+    }
+  });
+
+}
